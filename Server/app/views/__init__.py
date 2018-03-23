51c6888555008951fb2a1f782f3de1715a860227
JoMingyu <city7310@naver.com>
2018-03-23 10:42:05 +0900

    ADD: json_required

--- Server/app/views/__init__.py
+++ Server/app/views/__init__.py
@@ -1,3 +1,27 @@
+from functools import wraps
+
+from flask import abort, request
+
+
+def json_required(*required_keys):
+    def decorator(fn):
+        if fn.__name__ == 'get':
+            print('[WARN] JSON with GET method? on "{}()"'.format(fn.__qualname__))
+
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            if not request.is_json:
+                abort(406)
+
+            for required_key in required_keys:
+                if required_key not in request.json:
+                    abort(400)
+
+            return fn(*args, **kwargs)
+        return wrapper
+    return decorator
+
+
 class Router(object):
     """
     REST resource routing helper class like standard flask 3-rd party libraries
